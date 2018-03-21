25236b43639ceab3072523858639c77b98079b81
rullec <rullec@github.com>
2018-03-21 22:12:02 +0800

    emm... just I got it wrong

--- DataLoader.py
+++ DataLoader.py
@@ -3,43 +3,108 @@
 # author: rullec
 # date: 2018/3/20
 # version: v0.1
+
+import os
 import sys
 import traceback
+import random
+import numpy as np
 import tensorflow as tf
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.imagenet_utils import decode_predictions
 
 reload(sys)
 sys.setdefaultencoding('utf-8') 
-
-# 
-filenames=['images/000001.jpg','images/000002.jpg','images/000003.jpg','images/000004.jpg']
-labels=[1,0,1,0]
-
-filename_queue=tf.train.string_input_producer(filenames)
-
-reader=tf.WholeFileReader()
-filename, content = reader.read(filename_queue)
-images=tf.image.decode_jpeg(content, channels=3)
-images=tf.cast(images, tf.float32)
-resized_images=tf.image.resize_images(images, 224, 224)
-
-image_batch, label_batch=tf.train.batch([resized_images, labels], batch_size=2)
-print image.eval(session=tf.Session())  
 
 class DataLoader(object):
 	'''
 	This class will read every subdir in datadir as an training example.
 	an img LIST and an label LIST will be used to chareactrize an example, their index makes them one-to-one correspondence.
-	'''
-	def __init__(self, datadir)
-		try: 
-			print('DataLoader start...read data from %s.' % datadir)
-			subdirs, files = get_dirinfo(datadir)
-			print('there are %d examples in %s.' % (len(subdirs), datadir))
-			for i in range(len(subdirs)):
-				
-		except Exception as e:
-			traceback.print_exec()
-			# How to process these exceptions will be disuceesed in future.
 
-	def get_dirinfo(self, datadir)
-	
+	static vars:
+		subdirs: stored all of the subdirs' path in datadir
+		nowpos: stored next data we should load, used for batches.
+	'''
+	def get_dirinfo(self, path):
+		filelist = []
+		dirlist = []
+		# if this path exists and it is a dir(not a file)
+		if (os.path.exists(path) and os.path.isdir(path)):
+			files = os.listdir(path)
+			for file in files:
+				m = os.path.join(path, file)  
+				# m is dir or file?
+				if (os.path.isdir(m)):    
+					dirlist.append(m)
+				else:
+					filelist.append(m)
+		return dirlist, filelist
+
+	def __init__(self, datadir):
+		'''
+		this function can get all subdir paths and stored them.
+		parameters:
+			datadir: path to datadir, such as "./video_dir"
+		'''
+		print('DataLoader start, reading data from %s.' % datadir)
+		txtpath = './utils/subdirs.txt' # the index file that stored subdir paths
+		subdirs = []
+		if os.path.exists(txtpath):
+			with open(txtpath, 'r') as f:
+				index = f.readlines()
+				for i in range(0, len(index)):
+					subdirs.append(index[i][0:-1])
+		else:
+			subdirs, _ = self.get_dirinfo(datadir)
+			with open('./utils/subdirs.txt', 'w') as f:
+				for i in subdirs:
+					f.write(i+'\n')
+		print('there are %d examples in %s.' % (len(subdirs), datadir))
+		self.subdirs = subdirs
+		self.nowpos = 0
+
+	def get_next_batch(self, batch_size=32):
+		'''
+	this function aims at get next batch.
+	'''
+		left = self.nowpos
+		right = self.nowpos + batch_size
+		if right>len(self.subdirs):
+			right = self.subdirs
+		label = []
+		img = []
+		for i in range(left, right):
+			try:
+				path = self.subdirs[i]
+				_, files = self.get_dirinfo(path)
+				# get the ith example label
+				label.append((int)(path[-2:]))
+				# get the ith example images
+				subdata = []
+				for j in files:
+					if '.jpg'==j[-4:]:
+						jpg = load_img(j)
+						np_image = img_to_array(jpg)
+						subdata.append(np_image)
+						#print(np_image.shape)
+				img.append(subdata)
+				print('load %d sample succ, jpg num is %d.' % (i, len(subdata)))
+			except Exception as e:
+				traceback.print_exc()
+				continue
+			# How to process these exceptions will be disuceesed in future.
+		self.nowpos = right
+		return img, label
+
+	def shuffle(self):
+		'''
+	this function can help you shuffle datasets, after any epoch.
+	'''
+		random.shuffle(self.subdirs)
+
+if __name__ =='__main__':
+	'DataLoader.py test start!!!'
+	loader = DataLoader('./video_data')
+	img,label = loader.get_next_batch()
+	loader.shuffle()
